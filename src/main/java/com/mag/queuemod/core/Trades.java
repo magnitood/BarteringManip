5d4191fdc9f6c3bb920718e2805cedbe39503722
MaG-NITud3 <theeparthasarathi@gmail.com>
2022-08-12 16:14:50 +0530

    update barters to 1.16.5

--- src/main/java/com/mag/queuemod/core/Trades.java
+++ src/main/java/com/mag/queuemod/core/Trades.java
@@ -13,23 +13,24 @@ import net.minecraft.world.item.enchantment.Enchantments;
 import java.util.Collections;
 
 public enum Trades {
-    ENDER_PEARL(new ItemStack(Items.ENDER_PEARL, 8), "ender_pearl"),
-    OBSIDIAN(new ItemStack(Items.OBSIDIAN),  "obsidian"),
-    STRING(new ItemStack(Items.STRING, 24),  "string"),
-    IRON_NUGGET(new ItemStack(Items.IRON_NUGGET, 36), "iron_nugget"),
-    SOUL_SPEED_BOOK(ComplexItemStack.SOUL_SPEED_BOOK,  "soul_speed_book"),
+    ENDER_PEARL(new ItemStack(Items.ENDER_PEARL, 4), "ender_pearl"),
+    OBSIDIAN(new ItemStack(Items.OBSIDIAN,1), "obsidian"),
+    STRING(new ItemStack(Items.STRING, 9), "string"),
+    IRON_NUGGET(new ItemStack(Items.IRON_NUGGET, 36),"iron_nugget"),
+    SOUL_SPEED_BOOK(ComplexItemStack.SOUL_SPEED_BOOK, "soul_speed_book"),
     SOUL_SPEED_BOOTS(ComplexItemStack.SOUL_SPEED_BOOTS, "soul_speed_boots"),
     POTION_OF_FIRE_RESISTANCE(ComplexItemStack.POTION_OF_FIRE_RESISTANCE, "potion_of_fire_resistance"),
-    SPLASH_POTION_OF_FIRE_RESISTANCE(ComplexItemStack.SPLASH_POTION_OF_FIRE_RESISTANCE,  "splash_potion_of_fire_resistance"),
-    NETHER_QUARTZ(new ItemStack(Items.QUARTZ, 16),  "nether_quartz"),
-    GLOWSTONE_DUST(new ItemStack(Items.GLOWSTONE_DUST, 12), "glowstone_dust"),
-    MAGMA_CREAM(new ItemStack(Items.MAGMA_CREAM, 6),  "magma_cream"),
-    FIRE_CHARGE(new ItemStack(Items.FIRE_CHARGE, 5),  "fire_charge"),
+    SPLASH_POTION_OF_FIRE_RESISTANCE(ComplexItemStack.SPLASH_POTION_OF_FIRE_RESISTANCE, "splash_potion_of_fire_resistance"),
+    NETHER_QUARTZ(new ItemStack(Items.QUARTZ, 12), "nether_quartz"),
+    FIRE_CHARGE(new ItemStack(Items.FIRE_CHARGE, 1), "fire_charge"),
     GRAVEL(new ItemStack(Items.GRAVEL, 16), "gravel"),
-    LEATHER(new ItemStack(Items.LEATHER, 10),  "leather"),
-    SOUL_SAND(new ItemStack(Items.SOUL_SAND, 16),  "soul_sand"),
-    NETHER_BRICK(new ItemStack(Items.NETHER_BRICK, 16),  "nether_brick"),
-    CRYING_OBSIDIAN(new ItemStack(Items.CRYING_OBSIDIAN, 3),  "crying_obsidian");
+    LEATHER(new ItemStack(Items.LEATHER, 4),"leather"),
+    SOUL_SAND(new ItemStack(Items.SOUL_SAND, 8),"soul_sand"),
+    NETHER_BRICK(new ItemStack(Items.NETHER_BRICK, 8),"nether_brick"),
+    CRYING_OBSIDIAN(new ItemStack(Items.CRYING_OBSIDIAN,3),"crying_obsidian"),
+    SPECTRAL_ARROW(new ItemStack(Items.SPECTRAL_ARROW,12),"spectral_arrow"),
+    BLACKSTONE(new ItemStack(Items.BLACKSTONE,16),"blackstone"),
+    WATER_BOTTLE(ComplexItemStack.WATER_BOTTLE, "water_bottle");
 
     public final ItemStack barterItem;
     public final String name;
