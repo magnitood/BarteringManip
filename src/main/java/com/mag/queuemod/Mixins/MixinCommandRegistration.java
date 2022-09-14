b3ad1e8b65e855782f75d81beaf4a92d57086d27
MaG-NITud3 <theeparthasarathi@gmail.com>
2022-09-14 09:08:44 +0530

    changed command registration mixin logic

--- src/main/java/com/mag/queuemod/Mixins/MixinCommandRegistration.java
+++ src/main/java/com/mag/queuemod/Mixins/MixinCommandRegistration.java
@@ -4,19 +4,21 @@ import com.mag.queuemod.core.BarteringQueueCommand;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.server.commands.ExecuteCommand;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Mixin to register the bartering queue command
  */
 @Mixin(Commands.class)
 public abstract class MixinCommandRegistration {
-    @Redirect( at = @At( value = "INVOKE", target = "Lnet/minecraft/server/commands/ExecuteCommand;register(Lcom/mojang/brigadier/CommandDispatcher;)V") , method = "<init>")
-    private void injectCommand(CommandDispatcher<CommandSourceStack> commandDispatcher){
-        BarteringQueueCommand.register(commandDispatcher);
-        ExecuteCommand.register(commandDispatcher);
+    @Shadow @Final private CommandDispatcher<CommandSourceStack> dispatcher;
+    @Inject( at = @At("TAIL"), method = "<init>")
+    private void injectCommandRegistration(Commands.CommandSelection commandSelection, CallbackInfo ci){
+        BarteringQueueCommand.register(dispatcher);
     }
 }
